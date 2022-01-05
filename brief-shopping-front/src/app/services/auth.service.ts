de84ef43382d35b45c058ad07634608c90fe81f3
Fadima-doumbia <doumbia.fari@gmail.com>
2022-01-05 22:52:00 +0100

    first commit

---
+++ brief-shopping-front/src/app/services/auth.service.ts
@@ -0,0 +1,121 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { map } from 'rxjs/operators';
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { UserRequest } from '../models/payload/user.request';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  dev = false;
+  baseUrl = 'http://localhost:8080/api/auth';
+  API_URL?: string
+  private jwtHelper = new JwtHelperService();
+  URL_DEV = 'http://localhost:8080/api/auth';
+  jwtHelperService: any;
+
+  constructor(
+    private httpClient: HttpClient,
+    private router: Router  ) {
+    // this.API_URL = this.dev ? this.URL_DEV : this.URL_TEST;//pour dire si le dev utilise son ordinateur ou pas. si c'est le cas on utilise le localhost sinon on utilise l'url
+  }
+  //seul le dev se connecte avec le localhost tout les autres users utilisent le URL_TEST pour acceder au site
+  // ***************************************************************************************************************************
+  // login(user: UserRequest) {
+  //   // return this.httpClient.post(`${this.baseUrl}/login`, user)
+  //   return this.httpClient.post(`http://localhost:8080/api/auth/login`, user)
+  //   .pipe(
+  //     map((resp: any) => {
+  //       localStorage.setItem('TOKEN_APPLI', resp.accessToken);
+  //       localStorage.setItem('USER_ID', resp.id);
+  //       console.log(resp.id);
+  //       console.log('Token Save');
+  //       return resp;
+  //     })
+  //   );
+  // }
+
+  login(user: UserRequest) {
+    return this.httpClient.post(`${this.URL_DEV}/login`, user)
+    .pipe(
+      map((resp: any) => {
+        localStorage.setItem('TOKEN_APPLI', resp.accessToken);
+        localStorage.setItem('USER_ID', resp.id);
+        // console.log(localStorage.setItem('TOKEN_APPLI', resp.accessToken));
+        console.log(resp.id);
+        console.log('Token Save');
+        return resp;
+      })
+    );
+  }
+//********************************************************************************************************************* */
+
+  getUserIdToken() {
+    const userId  = localStorage.getItem('USER_ID');
+    if(userId){
+      return parseInt(userId);
+    }
+    return null;
+  }
+  // ***************************************************************************************************************************
+  getToken(){
+      const token:any =localStorage.getItem('TOKEN_APPLI');
+      console.log(token)
+      if(token){
+        return token;
+      }
+  }
+  // ***************************************************************************************************************************
+  getCurrentUser(){
+      const user_id:any =localStorage.getItem('USER_ID');
+      if(user_id){
+        return user_id;
+      }
+  }
+
+  // ***************************************************************************************************************************
+  getUserId(){
+    const helper = new JwtHelperService;
+    const decodedToken = helper.decodeToken(this.getToken());
+    console.log(decodedToken);
+    const id = parseInt(decodedToken.sub);
+    return id;
+  }
+
+  // ********************************************************************************************************************************
+  getUserTokenRole(){
+    const token:any = localStorage.getItem("TOKEN_APPLI");
+    const decode = this.jwtHelper.decodeToken(token);
+    // console.log( decode);
+    if (decode !== null) {
+      if (!this.jwtHelper.isTokenExpired(token)) {
+        return { ...decode, token };
+      } else {
+        localStorage.removeItem('TOKEN_APPLI');
+      }
+    }
+    return null;
+  }
+
+  // ***************************************************************************************************************************
+  newAdmin(newAdmin: UserRequest) {
+    return this.httpClient.post
+    (`${this.baseUrl}/register`, newAdmin)
+  }
+
+// ***************************************************************************************************************************
+
+
+  register(newUser: UserRequest) {
+    return this.httpClient.post(`http://localhost:8080/api/auth/register`, newUser)
+  }
+
+// ***************************************************************************************************************************
+  logout() {
+    localStorage.removeItem('TOKEN_APPLI');
+    this.router.navigate(['/bye']);
+  }
+}
---
+++ brief-shopping-front/src/app/services/user.service.ts
@@ -0,0 +1,56 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+import { SearchProjetRequest } from '../models/payload/search-projet.request';
+import { UserRequest } from '../models/payload/user.request';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  private baseUrl: string = "http://localhost:8080/api/users"
+
+  userSubject = new Subject<User[]>();
+
+  constructor(private httpClient: HttpClient) { }
+
+  create(newUser: UserRequest){
+    const body=JSON.stringify(newUser);
+    const formData=new FormData();
+    for(const [key, value] of Object.entries(newUser)){
+      formData.append(key,value)
+    }
+    console.log(body);
+    return this.httpClient.post(this.baseUrl, newUser)
+  }
+
+  getById(id: number) {
+    return this.httpClient.get<User>(`${this.baseUrl}/${id}`);
+  }
+
+  getUserById(id: number) {
+    return this.httpClient.get<User>(`${this.baseUrl}/${id}`);
+  }
+
+  getAllUser():Observable<Array<UserRequest>>  {
+    return this.httpClient.get<Array<UserRequest>>(`${this.baseUrl}`);
+  }
+
+  getByUsername(username: string) {
+    return this.httpClient.get<UserRequest>(`${this.baseUrl}/${username}`);
+  }
+
+  searchUser(search: SearchProjetRequest){
+    return this.httpClient.post<Array<User>>(`${this.baseUrl}/searchProject`, search);
+  }
+
+  updateUser(updateUser: User) {
+    return this.httpClient.put(`${this.baseUrl}/modif`, updateUser);
+  }
+
+  delete(id : number){
+    return this.httpClient.delete(`${this.baseUrl}/delete/` + id);
+  }
+}
